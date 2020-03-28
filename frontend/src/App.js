531e43ae362b5398cab74a429b6f045a1691297e
proggustavo <prog.gustavosantos@gmail.com>
2020-03-28 15:31:31 -0300

    Limpando projeto e construindo interface Web

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,22 +1,13 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          Hello omnistack
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+import './global.css';
+
+import Logon from './pages/Logon';
+
+function App() {
+     return (
+      <Logon />
     );
-  }
 }
 
 export default App;
